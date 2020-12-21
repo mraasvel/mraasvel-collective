182360fbee9bcb5af5885b72e5e625fb3e8688db
mraasvel <mbraasveldt@gmail.com>
2020-12-22 00:39:10 +0100

    automatic push

--- get_pathnames/main.c
+++ get_pathnames/main.c
@@ -6,14 +6,23 @@
 /*   By: mraasvel <mraasvel@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/12/21 19:41:52 by mraasvel      #+#    #+#                 */
-/*   Updated: 2020/12/21 19:46:43 by mraasvel      ########   odam.nl         */
+/*   Updated: 2020/12/22 00:33:11 by mraasvel      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include "head.h"
 
-// /home/mraasvel/work/mraasvel-collective/art
+# define PATH "../art/"
+# define NAME "ascii-art-"
+# define SUFFIX ".txt"
+# define ART_COUNT 301
+
+/*
+** TO ADD NEW ART:
+** 1. change "# define FRAME_TOTAL", to new number
+** 2. copy paste everything in "pathnames.txt" into the variable filenames in ./program/main.c
+** 3. change the "# define FRAME_TOTAL" in ./program/include/ascii.h to new number
+*/
 
 int	main(void)
 {
